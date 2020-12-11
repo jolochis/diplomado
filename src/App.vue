b84141904af73a225f604729057961b8b544aff4
jolochis <jolochis@gmail.com>
2020-12-10 21:35:47 -0700

    done

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,10 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+
+  <HelloWorld />
 </template>
 
 <script>
+
 import HelloWorld from './components/HelloWorld.vue'
 
 export default {
@@ -15,12 +16,73 @@ export default {
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+input {
+	width: 89.7%;
+	height: 40px;  
+	white-space: pre;
+	word-break: break-word;
+	border-radius: 5px;
+}
+
+label{
+	display: inline-block;
+	width: 180px;
+}
+
+select{
+	width: 90%;
+	height: 40px;
+	border-radius: 5px;
+}
+
+h3{
+	text-align: 80px;
+	color: 	#0000CD;
+	font-family: Arial;
+	font-size: x-large;
+}
+form {
+	padding-left: 80px;
+}
+
+
+.button {
+	background-color: #00BFFF; 
+	border: none;
+	color: white;
+	padding: 10px 27px;
+	text-align: center;
+	text-decoration: none;
+	display: inline-block;
+	font-size: 16px;
+	border-radius: 10px;
+}
+
+table{
+	border:1;
+	width: 100%;
+	border-collapse:separate
+}
+
+
+.F0{
+	background-color: #4169E1; 
+	color: white;
+	padding: 10px 40px;
+	text-decoration: none;  
+}
+
+.F1{
+	background-color: #A9A9A9; 
+	color: black;
+	padding: 10px 40px;
+	text-decoration: none;  
+}
+
+.F2{
+	background-color: #DCDCDC; 
+	color: black;
+	padding: 10px 40px;
+	text-decoration: none;  
 }
 </style>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,40 +1,121 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
-  </div>
+  <div class="container">
+    <h3>Alumnos del Diplomado</h3>
+	<hr>
+  <form @submit.prevent="agregar">
+		<label>Nombre:</label>
+			<input v-model="nombre" type="text" name="nombre" id="minombre" placeholder="Escribe tu nombre...">			
+
+		<label>Apellido:</label>
+			<input v-model="apellido" type="text" name="correo" id="micorreo" placeholder="Escribe tu apellido...">		
+
+		<label>Edad:</label> 
+			<input v-model.number="edad" type="text" name="numero" id="numero" placeholder="Captura tu edad...">		
+			
+		<label>Grado acádemico:</label>
+				<select v-model="grado" name="select">
+						<option value="secundaria" selected>Secundaria</option>
+						<option value="primaria" >Primaria</option>
+						<option value="licenciatura">Licenciatura</option>
+						<option value="doctorado">Doctorado</option>
+						<option value="tecnica">Carrera Técnica</option>
+						<option value="maestria">Maestria</option>
+						<option value="postgrado">Posgrado</option>
+				</select>
+<br/>				
+		<label>Lenguaje de Programación Favorito:</label>
+				<select v-model="lenguaje" name="select">
+						<option value="javascript" selected>JavaScript</option>
+						<option value="php" >PHP</option>
+						<option value="c#">C#</option>
+						<option value="c++">C++</option>
+            <option value="python">Python</option>
+            <option value="java">Java</option>
+				</select>
+<div class="form-group mt-2">
+<button class="btn btn-success"> Agregar </button>
+</div>
+	
+  
+</form>
+<br/>
+<br/>
+<br/>
+
+
+  <table class="table">
+  <thead>
+    <tr>
+      <th scope="col">Nombre</th>
+      <th scope="col">Apellido</th>
+      <th scope="col">Edad</th>
+      <th scope="col">Grado</th>
+      <th scope="col">Lenguaje</th>
+      <th scope="col"> Acciones</th>
+    </tr>
+  </thead>
+  <tbody >
+    <tr v-for="(alumno, index) in alumnos" :key="index">
+      <td> {{alumno.nombre}} </td>
+      <td>{{alumno.apellido}}</td>
+      <td>{{alumno.grado}}</td>
+      <td>{{alumno.edad}}</td>
+      <td>{{alumno.lenguaje}}</td>
+      <button class="btn btn-danger" @click="eliminar(index)">X</button>
+    </tr>
+ 
+  </tbody>
+</table>
+  
+</div>
 </template>
 
 <script>
+
 export default {
   name: 'HelloWorld',
-  props: {
-    msg: String
+ 
+  data () {
+    return {
+      nombre: '',
+      apellido: '',
+      edad : null,
+      grado: '',
+      alumnos: [
+        {"nombre": "Jose", "apellido": "Lopez", "edad" : 22, "grado": "Preparatoria", "lenguaje" : "PHP"},
+        {"nombre": "David", "apellido": "Olea", "edad" : 32, "grado": "Maestria", "lenguaje" : "C++"},
+        {"nombre": "Maria", "apellido": "Miller", "edad" : 25, "grado": "Licenciatura", "lenguaje" : "Python"},
+        {"nombre": "Efraín", "apellido": "Rincon", "edad" : 27, "grado": "Licenciatura", "lenguaje" : "Java"},
+        {"nombre": "Mario", "apellido": "Cruz", "edad" : 24, "grado": "Licenciatura", "lenguaje" : "Python"},
+        {"nombre": "Luis", "apellido": "Espinoza", "edad" : 26, "grado": "Licenciatura", "lenguaje" : "PHP"},
+        {"nombre": "Alejandro", "apellido": "Juarez", "edad" : 35, "grado": "Licenciatura", "lenguaje" : "C"},
+        {"nombre": "Marcos", "apellido": "Castro", "edad" : 33, "grado": "Maestria", "lenguaje" : "C#"},
+        {"nombre": "Javier", "apellido": "Guardado", "edad" : 22, "grado": "Licenciatura", "lenguaje" : "PHP"},
+        {"nombre": "Jesus", "apellido": "Veliz", "edad" : 20, "grado": "Licenciatura", "lenguaje" : "JavaScript"},
+      ]
+    }
+  },
+  methods: {
+    agregar(){
+      let nuevo = {
+        nombre : this.nombre,
+        apellido : this.apellido,
+        edad : this.edad,
+        grado : this.grado,
+        lenguaje : this.lenguaje
+      }
+      this.$emit('submit', nuevo)
+      this.alumnos.push(nuevo)
+      this.nombre = ''
+      this.apellido = ''
+      this.edad = null
+      this.grado = ''
+      this.lenguaje = ''
+    },
+    eliminar(index){
+      this.alumnos.splice(index,1)
+      console.log(index)
+    }
   }
 }
 </script>
